0386ac46313b61b6ff8d3512f4c927b9c8fdae94
笈川可可 <0xbbc@0xbbc.com>
2017-12-21 12:22:06 +0800

    风格统一、代码对齐，修改部分使之更易阅读w

--- Coursera_Assignments/12.3 最远距离.cpp
+++ Coursera_Assignments/12.3 最远距离.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-
 using namespace std;
+
 int main(int argc, char *argv[]) {
-int n = 0;
-	while (cin >> n) {
-		double * xs = new double[n], * ys = new double[n];
-		int t = 0;
-		while (t != n) {
-			double x, y;
-			cin >> x >> y;
-			xs[t] = x;
-			ys[t] = y;
-			t++;
-		}
-		double dis = 0;
-		for (int i = 0;i < n - 1;i++) {
-			for (int j = i;j < n;j++) {
-				double x1 = xs[i], y1 = ys[i];
-				double x2 = xs[j], y2 = ys[j];
-				double distance = sqrt (pow(x1 - x2, 2) + pow(y1 - y2, 2));
-				if (distance > dis) {
-					dis = distance;
-				}
-			}
-		}
-		cout << fixed << setprecision(4) << dis << endl;
-	}
-	
+    int n = 0;
+    while (cin >> n) {
+        double * xs = new double[n], * ys = new double[n];
+        int t = 0;
+        while (t != n) {
+            double x, y;
+            cin >> x >> y;
+            xs[t] = x;
+            ys[t] = y;
+            t++;
+        }
+        double dis = 0;
+        for (int i = 0;i < n - 1;i++) {
+            for (int j = i;j < n;j++) {
+                double x1 = xs[i], y1 = ys[i];
+                double x2 = xs[j], y2 = ys[j];
+                double distance = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+                if (distance > dis) {
+                    dis = distance;
+                }
+            }
+        }
+        cout << fixed << setprecision(4) << dis << endl;
+        delete [] xs;
+        delete [] ys;
+    }
+    
 }
